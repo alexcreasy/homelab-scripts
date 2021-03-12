d10cee92b6b517b8ff5a7c31f943e6e078221805
Alex Creasy <acreasy@redhat.com>
2021-03-12 16:11:44 +0000

    More scripts added

---
+++ install-docker-debian.sh
@@ -0,0 +1,35 @@
+ #!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You must be root to do this." 1>&2
+    exit 100
+fi
+
+apt-get update
+
+apt-get remove docker docker-engine docker.io containerd runc
+
+apt-get install \
+	apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+docker run hello-world
+
+
+UID 110
+GID 1002
